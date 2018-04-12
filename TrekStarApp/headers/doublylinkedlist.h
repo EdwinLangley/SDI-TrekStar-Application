7a8f19b97bc4fba20688b3c217c9429096f35367
Edwin <Edlangley@live.co.uk>
2018-04-12 17:25:24 +0100

    Added some more linked list functions

--- TrekStarApp/headers/doublylinkedlist.h
+++ TrekStarApp/headers/doublylinkedlist.h
@@ -2,6 +2,7 @@
 #define DOUBLYLINKEDLIST_H
 
 #include "node.h"
+#include "project.h"
 
 
 
@@ -20,6 +21,21 @@ public:
 };
 
 void createNewNode(Project project);
+
+void displayAllNodes();
+
+void insert_start(Project value);
+
+void insert_position(int pos, Project value);
+
+void delete_first();
+
+void delete_last();
+
+void delete_position(int pos);
+
+
+
 
 
 #endif // DOUBLYLINKEDLIST_H
--- TrekStarApp/src/doublylinkedlist.cpp
+++ TrekStarApp/src/doublylinkedlist.cpp
@@ -22,3 +22,72 @@ void DoublyLinkedList::createNewNode(Project project)
       tail=temp;
     }
   }
+
+void DoublyLinkedList::displayAllNodes() {
+    node *temp=new node;
+    temp=head;
+    while(temp!=NULL)
+    {
+      cout<<temp->data<<"\t";
+      temp=temp->next;
+    }
+  }
+
+
+void DoublyLinkedList::insert_start(Project value){
+  node *temp=new node;
+  temp->data=value;
+  temp->next=head;
+  head=temp;
+}
+
+void DoublyLinkedList::insert_position(int pos, Project value){
+  node *pre=new node;
+  node *cur=new node;
+  node *temp=new node;
+  cur=head;
+  for(int i=1;i<pos;i++)
+  {
+    pre=cur;
+    cur=cur->next;
+  }
+  temp->data=value;
+  pre->next=temp;
+  temp->next=cur;
+}
+
+void DoublyLinkedList::delete_first()
+ {
+   node *temp=new node;
+   temp=head;
+   head=head->next;
+   delete temp;
+ }
+
+void DoublyLinkedList::delete_last()
+{
+  node *current=new node;
+  node *previous=new node;
+  current=head;
+  while(current->next!=NULL)
+  {
+    previous=current;
+    current=current->next;
+  }
+  tail=previous;
+  previous->next=NULL;
+  delete current;
+}
+
+void DoublyLinkedList::delete_position(int pos)
+ {
+   node *current=new node;
+   node *previous=new node;
+   current=head;
+   for(int i=1;i<pos;i++)
+   {
+     previous=current;
+     current=current->next;
+   }
+   previous->next=current->next;
+ }
