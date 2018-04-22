25fa9b80ef7ed176631408e865f2fccc3a277722
Edward Celella <edwardcelella@gmail.com>
2018-04-23 00:12:12 +0100

    Functionality added to clear add and delete button on main screen

--- TrekStarApp/src/controller/controller.cpp
+++ TrekStarApp/src/controller/controller.cpp
@@ -9,21 +9,17 @@ controller::controller()
 {
 
     mw.ui->cbGenre->addItem("Action");
-    mw.ui->cbGenre->addItem("Horror");
+    mw.ui->cbGenre->addItem("Comedy");
     mw.ui->cbGenre->addItem("Drama");
+    mw.ui->cbGenre->addItem("Documentary");
+    mw.ui->cbGenre->addItem("Horror");
+    mw.ui->cbGenre->addItem("Thriller");
 
     mw.ui->cbLanguage->addItem("English");
     mw.ui->cbLanguage->addItem("French");
     mw.ui->cbLanguage->addItem("German");
-
-    mw.ui->lstLocations->addItem("locTest1");
-    mw.ui->lstLocations->addItem("locTest2");
-    mw.ui->lstLocations->addItem("locTest3");
-
-    mw.ui->lstKeywords->addItem("keyTest1");
-    mw.ui->lstKeywords->addItem("keyTest2");
-    mw.ui->lstKeywords->addItem("keyTest3");
-
+    mw.ui->cbLanguage->addItem("Spanish");
+    mw.ui->cbLanguage->addItem("Mandarin");
 
     connect(mw.ui->cmdCreate, SIGNAL (clicked()), this, SLOT (handleCreateProject()));
     connect(mw.ui->cmdClear, SIGNAL (clicked()), this, SLOT (handleClear()));
@@ -50,12 +46,12 @@ void controller::handleCreateProject(){
     int sales = mw.ui->sbSales->value();
 
     std::vector<std::string> locations;
-    for(int i = 0; i < mw.ui->lstLocations->count(); ++i){
+    for(unsigned int i = 0; i < mw.ui->lstLocations->count(); ++i){
         locations.push_back(mw.ui->lstLocations->item(i)->text().toStdString());
     }
 
     std::vector<std::string> keywords;
-    for(int i = 0; i < mw.ui->lstKeywords->count(); ++i){
+    for(unsigned int i = 0; i < mw.ui->lstKeywords->count(); ++i){
         keywords.push_back(mw.ui->lstKeywords->item(i)->text().toStdString());
     }
 
@@ -101,37 +97,73 @@ void controller::handleCreateProject(){
         input.setWeeklyBoxFigures(sales);
 
         projList.createnode(input);
-
         openProj = &projList.findByTitle(projTitle);
+        pw.show();
 
-        openProj->setGenre("lol");
-
-        std::cout << "Project created" << std::endl;
-
-        std::cout << projList.findByTitle(projList.getAllFilmTitles().at(0)).getGenre() << std::endl;
+        handleClear();
 
      }
 
-    pw.show();
-
 }
 
+// Clears form
 void controller::handleClear(){
-    std::cout << "clear" << std::endl;
+
+    mw.ui->txtTitleProject->clear();
+    mw.ui->txtSummary->clear();
+    mw.ui->cbGenre->setCurrentIndex(0);
+    mw.ui->cbLanguage->setCurrentIndex(0);
+    mw.ui->deRelease->setDate(QDate(2000, 1, 1));
+    mw.ui->sbRuntime->setValue(0);
+    mw.ui->sbSales->setValue(0);
+    mw.ui->lstLocations->clear();
+    mw.ui->lstKeywords->clear();
+
+    mw.ui->lblTitleProjects->setStyleSheet("color: #78CAD2");
+    mw.ui->lblSummary->setStyleSheet("color: #78CAD2");
+    mw.ui->lblLocations->setStyleSheet("color: #78CAD2");
+    mw.ui->lblKeywords->setStyleSheet("color: #78CAD2");
 }
 
+// Adds the input from the text box to the location list
 void controller::handleLocationAdd(){
-    std::cout << "Loc add" << std::endl;
+
+    QString input = mw.ui->txtLocationAdd->text();
+    if(input.toStdString() != ""){
+        mw.ui->lstLocations->addItem(input);
+    }
+    mw.ui->txtLocationAdd->clear();
 }
 
+// Deletes selected items from the location list
 void controller::handleLocationDel(){
-    std::cout << "Loc del" << std::endl;
+
+    QList <QListWidgetItem *> selectedItems = mw.ui->lstLocations->selectedItems();
+    for(unsigned int i = 0; i < selectedItems.size(); i++){
+        std::cout << i << std::endl;
+        delete mw.ui->lstLocations->takeItem(mw.ui->lstLocations->row(selectedItems[i]));
+    }
+
 }
 
+// Adds the input from the text box to the keywords list
 void controller::handleKeywordsAdd(){
-    std::cout << "Key add" << std::endl;
+
+    QString input = mw.ui->txtKeywordsAdd->text();
+    if(input.toStdString() != ""){
+        mw.ui->lstKeywords->addItem(input);
+    }
+    mw.ui->txtKeywordsAdd->clear();
+
 }
 
+// Deletes selected items from the keywords list
 void controller::handleKeywordsDel(){
-    std::cout << "Key del" << std::endl;
+
+    QList <QListWidgetItem *> selectedItems = mw.ui->lstKeywords->selectedItems();
+    for(unsigned int i = 0; i < selectedItems.size(); i++){
+        std::cout << i << std::endl;
+        delete mw.ui->lstKeywords->takeItem(mw.ui->lstKeywords->row(selectedItems[i]));
+    }
+
 }
